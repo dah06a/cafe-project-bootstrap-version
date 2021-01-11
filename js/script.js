232a058f323e89e874f2965b105c65db7f635c30
David Henry <dah06a@acu.edu>
2021-01-10 23:00:24 -0600

    Snake game basics all working!

--- js/script.js
+++ js/script.js
@@ -11,9 +11,30 @@ let leftKey = false;
 let rightKey = false;
 
 //Game Global Variables
-let gameStarted = true;
+let gameOver = false;
+let gameSpeed = 1; //How far snake moves each frame
+let gameSize = 8; //Determines size of snake and apples on screen
+let drawSpeed = 10; //Draw function interval speed - lower number = faster
+let apples = [];
 const w = canvas.width;
 const h = canvas.height;
+
+//HTML Variables
+const gameOverTitle = document.getElementById("gameOverTitle");
+const gameOverBtn = document.getElementById("gameOverBtn");
+gameOverBtn.addEventListener("click", resetGame);
+function resetGame() {
+    gameOver = false;
+    snake.headX = w/2;
+    snake.headY = h/2;
+    snake.bodX = [];
+    snake.bodY = [];
+    snake.bodLen = 200;
+    snake.goingUp = false;
+    snake.goingDown = false;
+    snake.goingLeft = false;
+    snake.goingRight = true;
+}
 
 //Document Event Functions Listening For Key Press
 function onKeyDown(e) {
@@ -37,12 +58,42 @@ $(document).on("keyup", onKeyUp);
 $(function() {
     if (canvas.getContext) {
         ctx = canvas.getContext("2d");
-        console.log(canvas);
-        setInterval("draw()", 10);
+        gameOverTitle.style.display = "none";
+        gameOverBtn.style.display = "none";
+        setInterval("draw()", drawSpeed);
     } else {
         console.log("Error:  Canvas unsupported, cannot get context.")
     }
 });
+
+//Random Canvas Position Helper Functions
+function randomX() {
+    return Math.floor(Math.random()*(w-10)) + 10;
+}
+function randomY() {
+    return Math.floor(Math.random()*(h-10)) + 10;
+}
+
+//Apple Constructor Function
+function Apple(posX, posY) {
+    this.posX = posX;
+    this.posY = posY;
+    this.size = gameSize-2;
+    this.color = "rgb(200, 25, 25)";
+}
+
+//Apple Display Function
+Apple.prototype.display = function() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.posX, this.posY, this.size, 0, 2*Math.PI);
+    ctx.fill();
+}
+
+//Create Multiple Apples
+for (let i = 0; i < 5; i++) {
+    apples.push(new Apple( randomX(), randomY() ) );
+}
 
 //Snake Constructor Function
 function Snake(headX, headY) {
@@ -50,8 +101,9 @@ function Snake(headX, headY) {
     this.headY = headY;
     this.bodX = [];
     this.bodY = [];
-    this.bodLen = 50;
-    this.hasCollided = false;
+    this.bodLen = 200;
+    this.bodSize = gameSize;
+    this.color = "rgb(25, 100, 25)";
     this.goingUp = false;
     this.goingDown = false;
     this.goingLeft = false;
@@ -60,10 +112,10 @@ function Snake(headX, headY) {
 
 //Snake Display Function
 Snake.prototype.display = function() {
-    ctx.fillStyle = "rgb(25, 100, 25)";
+    ctx.fillStyle = this.color;
     for (let i = 0; i < this.bodX.length; i++) {
         ctx.beginPath();
-        ctx.arc(this.bodX[i], this.bodY[i], 5, 0, 2*Math.PI, true);
+        ctx.arc(this.bodX[i], this.bodY[i], this.bodSize, 0, 2*Math.PI);
         ctx.fill();
     }
 }
@@ -71,10 +123,10 @@ Snake.prototype.display = function() {
 //Snake Movement Update Function
 Snake.prototype.update = function() {
     //Move snake in the direction it is already going
-    if (this.goingUp) this.headY -= 1;
-    if (this.goingDown) this.headY += 1;
-    if (this.goingLeft) this.headX -= 1;
-    if (this.goingRight) this.headX += 1;
+    if (this.goingUp) this.headY -= gameSpeed;
+    if (this.goingDown) this.headY += gameSpeed;
+    if (this.goingLeft) this.headX -= gameSpeed;
+    if (this.goingRight) this.headX += gameSpeed;
 
     //Add the new coordinates to the snake body
     this.bodX.push(this.headX);
@@ -110,16 +162,42 @@ Snake.prototype.update = function() {
 }
 
 //Snake Collision Checking
+Snake.prototype.checkCollisions = function() {
+
+    //Check for walls/border of canvas
+    if (this.headY <= this.bodSize || this.headY >= h-this.bodSize || this.headX <= this.bodSize || this.headX >= w-this.bodSize) gameOver = true;
+
+    //Check for collisions with snake body
+    for (let i = this.bodSize; i < this.bodX.length; i++) {
+        if (this.goingUp && this.headX === this.bodX[i] && this.headY-this.bodSize === this.bodY[i]) gameOver = true;
+        if (this.goingDown && this.headX === this.bodX[i] && this.headY+this.bodSize === this.bodY[i]) gameOver = true;
+        if (this.goingLeft && this.headY === this.bodY[i] && this.headX-this.bodSize === this.bodX[i]) gameOver = true;
+        if (this.goingRight && this.headY === this.bodY[i] && this.headX+this.bodSize === this.bodX[i]) gameOver = true;
+    }
+
+    //Check for apple collisions
+    for (let i = 0; i < apples.length; i++) {
+        if (Math.abs(this.headX - apples[i].posX) < this.bodSize && Math.abs(this.headY - apples[i].posY) < this.bodSize) {
+            apples[i].posX = randomX();
+            apples[i].posY = randomY();
+            this.bodLen += 100;
+        }
+    }
+}
 
 let snake = new Snake(w/2, h/2);
 
-function clearCanvas() {
-    ctx.clearRect(0, 0, w, h);
-}
-
 function draw() {
-    clearCanvas();
-    snake.display();
-    snake.update();
-    console.log(upKey, downKey, leftKey, rightKey);
+    if (gameOver) {
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, w, h);
+        gameOverTitle.style.display = "inline-block";
+        gameOverBtn.style.display = "inline-block";
+    } else {
+        ctx.clearRect(0, 0, w, h);
+        for (let i = 0; i < apples.length; i++) apples[i].display();
+        snake.display();
+        snake.update();
+        snake.checkCollisions();
+    }
 }
